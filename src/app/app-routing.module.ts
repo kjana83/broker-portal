a3ca1afedf8145e9676f5dc32cd50b1693c37fdb
Janarthanan K <janarthanan.kanagaraj@gmail.com>
2020-01-02 20:30:18 -0500

    Merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { BMSModule } from './BMS/bms.module';
-
 
 const routes: Routes = [
   {
